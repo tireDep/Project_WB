16c6ea9d1fe9b53884c95ac066440495b9348761
DESKTOP-94M8TSN\wing9 <wing997@naver.com>
2025-08-25 23:16:43 +0900

    언리얼 프로젝트 생성

---
+++ Project_WB/Source/Project_WB/Project_WB.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_WB.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_WB, "Project_WB" );
